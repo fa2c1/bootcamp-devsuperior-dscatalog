71dc4e11da968f8ce110d138951a283182fd1531
Fernando Coutinho <fa2c1@hotmail.com>
2022-03-23 21:01:06 -0300

    Nivelamento de rotas com o projeto do Bootcamp

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,27 +1,11 @@
-import './assets/styles/custom.scss';
+import 'assets/styles/custom.scss';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Navbar from 'components/Navbar';
-import Home from 'pages/Home';
-import Catalog from 'pages/Catalog';
-import Admin from 'pages/Admin';
+
+import Routes from 'Routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Switch>
-        <Route path="/" exact>
-          <Home />
-        </Route>
-        <Route path="/products">
-          <Catalog />
-        </Route>
-        <Route path="/admin">
-          <Admin />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <Routes />
   );
 }
 
