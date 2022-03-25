a090f712f31994c27ebaddfa7f225758b04594b4
Fernando Coutinho <fa2c1@hotmail.com>
2022-03-25 19:47:49 -0300

    Catalog Title

--- frontweb/src/pages/Catalog/index.tsx
+++ frontweb/src/pages/Catalog/index.tsx
@@ -2,6 +2,8 @@ import ProductCard from "components/ProductCard";
 import { Link } from "react-router-dom";
 import { Product } from "types/product";
 
+import "./styles.css"
+
 function Catalog() {
 
     const product: Product = {
@@ -24,7 +26,10 @@ function Catalog() {
     }
 
     return (
-        <div className="container my-4">
+        <div className="container my-4 catalog-container">
+            <div className="row catalog-title-container">
+                <h1>Catalago de Produtos</h1>
+            </div>
             <div className="row">
                 <div className="col-sm-6 col-lg-4 col-xl-3">
                     <Link to="/products/1">
