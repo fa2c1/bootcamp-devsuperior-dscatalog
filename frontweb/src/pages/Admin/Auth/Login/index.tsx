d57010e81392fcb79374a3297fe8e1727e493b18
Fernando Coutinho <fa2c1@hotmail.com>
2022-04-24 13:47:13 -0300

    Conditional styling

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -38,11 +38,11 @@ function Login() {
             }
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="mb-4">
-                    <input {...register("username", {required: 'Campo Obrigatório', pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: 'E-mail invalido'}})} type="text" className="form-control base-input" placeholder="Email" name="username" />
+                    <input {...register("username", {required: 'Campo Obrigatório', pattern: {value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i, message: 'E-mail invalido'}})} type="text" className={`form-control base-input ${errors.username ? 'is-invalid' : ''}`} placeholder="Email" name="username" />
                     <div className='invalid-feedback d-block'>{errors.username?.message}</div>
                 </div>
                 <div className="mb-2">
-                    <input {...register("password", {required: 'Campo Obrigatório'})} type="password" className="form-control base-input " placeholder="Password" name="password" />
+                    <input {...register("password", {required: 'Campo Obrigatório'})} type="password" className={`form-control base-input ${errors.password ? 'is-invalid' : ''}`} placeholder="Password" name="password" />
                     <div className='invalid-feedback d-block'>{errors.password?.message}</div>
                 </div>
                     <Link to="/admin/auth/recover" className="login-link-recover">Esqueci a senha</Link>
