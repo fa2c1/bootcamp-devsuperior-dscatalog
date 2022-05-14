f2ab2d59206a9326fb9363bf8ad60e69c99ef13c
Fernando Coutinho <fa2c1@hotmail.com>
2022-05-14 14:43:42 -0300

    Login/Logout logic

--- frontweb/src/components/Navbar/index.tsx
+++ frontweb/src/components/Navbar/index.tsx
@@ -1,14 +1,43 @@
 import './styles.css';
 import 'bootstrap/js/src/collapse.js';
 import { Link, NavLink } from 'react-router-dom';
+import { getTokenData, isAuthenticated, removeAuthData, TokenData } from 'util/requests';
+import { useEffect, useState } from 'react';
+import history from 'util/history';
+
+type AuthData = {
+    autenticated: boolean;
+    tokenData?: TokenData;
+}
 
 function Navbar() {
+    const [authData, setAuthData] = useState<AuthData>({autenticated: false});
+
+    useEffect(()=>{
+        if(isAuthenticated()){
+            setAuthData({
+                autenticated: true,
+                tokenData: getTokenData()
+            });
+        }else{
+            setAuthData({
+                autenticated: false
+            });
+        }
+    }, []);
+
+    const handleLogoutClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
+        event.preventDefault();
+        removeAuthData();
+        setAuthData({
+            autenticated: false
+        });
+        history.replace('/');
+    }
+
     return (
         <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
             <div className="container-fluid">
-                <Link to="/" className='nav-logo-text'>
-                    <h4>DS Catalog</h4>
-                </Link>
                 <button
                     className="navbar-toggler"
                     type="button"
@@ -20,6 +49,9 @@ function Navbar() {
                 >
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                <Link to="/" className='nav-logo-text'>
+                    <h4>DS Catalog</h4>
+                </Link>
                 <div className="collapse navbar-collapse" id="dscatalog-navbar">
                     <ul className="navbar-nav offset-md-2 main-menu">
                         <li>
@@ -32,6 +64,14 @@ function Navbar() {
                             <NavLink to="/admin" activeClassName='active'>ADMIN</NavLink>
                         </li>
                     </ul>
+                </div>
+                <div>
+                    <span>{authData.tokenData?.user_name}</span>
+                    {authData.autenticated ? (
+                        <a href='#logout' onClick={handleLogoutClick}>LOGOUT</a>
+                    ) : (
+                        <Link to="/admin/auth">LOGIN</Link>
+                    )}
                 </div>
             </div>
         </nav>
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -5,7 +5,7 @@ import history from './history';
 
 type Role = 'ROLE_OPERATOR' | 'ROLE_ADMIN';
 
-type TokenData = {
+export type TokenData = {
     exp: number;
     user_name: string;
     authorities: Role[];
@@ -62,6 +62,10 @@ export const saveAuthData = (obj: LoginResponse) => {
 export const getAuthData = () => {
     const str = localStorage.getItem(tokenKey) ?? "{}";
     return JSON.parse(str) as LoginResponse;
+}
+
+export const removeAuthData = () => {
+    localStorage.removeItem(tokenKey);
 }
 
 export const getTokenData = () : TokenData | undefined => {
