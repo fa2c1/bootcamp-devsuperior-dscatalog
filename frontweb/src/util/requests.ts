4a77b02a5e8110cde51b66d4e04db681f40813e4
Fernando Coutinho <fa2c1@hotmail.com>
2022-05-18 20:02:31 -0300

    hasAnyRoles

--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -99,3 +99,17 @@ axios.interceptors.response.use(function (response) {
     // Create function Logic
     return Promise.reject(error);
   });
+
+  export const hasAnyRoles = (roles: Role[]) : boolean => {
+    if (roles.length === 0){
+        return true;
+    }
+
+    const tokenData = getTokenData();
+
+    if (tokenData !== undefined){
+        return roles.some(role => tokenData.authorities.includes(role));
+    }else{
+        return false;
+    }
+  }
